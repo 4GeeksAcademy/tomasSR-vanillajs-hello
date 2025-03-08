1bf63e9bfda6744467a3412475fe81e2dbcbc6b8
waloleitor <tsarciatroch@gmail.com>
2025-03-08 18:38:34 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,16 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let quien = ["Mi abuelo ", "Mi vecino Jose ", "Mi profesor ", "Un alien ", "Mi amiga Dolo ", "Joey ", "El fantasma del baño ", "Armando, el pato, ", "Mi padre "];
+let accion = ["se quedó atrapado en el ascensor ", "me secuestró ", "se desmayó ", "invadió mi casa ", "activó la alarma de incendios ", "perdió las llaves de mi casa ", "se comió mis deberes ", "me indicó mal la dirección ", "me retó a una partida de ajedrez "];
+let cuando= ["justo antes de salir de casa ", "cuando ya estaba de camino ", "a mitad de la noche ", "en el último minuto ","justo cuando me desperté ", "mientras estaba en el baño ", "justo cuando iba a pagar ","cuando mi jefe me estaba llamando ", "mientras me estaba vistiendo "];
+const no_pude = "y no pude";
+let marron = ["entregar los deberes.","pagar la cuenta.", "hacer ejercicio.", "sacar la basura.", "ir a la comida familiar.", "responder el mensaje","hacer la compra.", "ir a la cita con el dentista", "lavar el coche"];
+
+let variable1= Math.floor(Math.random() * quien.length);
+let variable2= Math.floor(Math.random() * accion.length);
+let variable3= Math.floor(Math.random() * cuando.length);
+let variable4= Math.floor(Math.random() * marron.length);
+
+document.querySelector(".excusa").innerHTML = quien[variable1] + accion[variable2] + cuando[variable3]+" y no pude "+ marron[variable4];
+}
